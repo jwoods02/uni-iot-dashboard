4d52a06f1039de0c1c591f183813dd75cc20cea5
James Woods <woodsj13@cardiff.ac.uk>
2018-11-19 15:03:39 +0000

    Setup login tests (not working)

---
+++ src/container/loginform/loginform.test.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { mount } from "enzyme";
+import LoginForm from "./loginform";
+
+describe("Login Form Tests", () => {
+  let props;
+  let mountedLogin;
+  const loginForm = () => {
+    if (!mountedLogin) {
+      mountedLogin = mount(<LoginForm {...props} />);
+    }
+    return mountedLogin;
+  };
+
+  beforeEach(() => {
+    props = {
+      memberData: []
+    };
+    mountedLogin = undefined;
+  });
+
+  // All tests will go here
+
+  it("always renders a div", () => {
+    const divs = loginForm().find("div");
+    expect(divs.length).toBeGreaterThan(0);
+  });
+});
