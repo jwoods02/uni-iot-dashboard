387d8c8547fbbe25a2f5bd846ce9b5653d7f6ec1
Christopher Cardwell <CardwellCJ@cardiff.ac.uk>
2018-11-20 11:56:03 +0000

    Changed the styling of the homepage and the buttons to improve colour

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Home from './components/Home';
-import Nav from './components/Nav';
-import TopNav from './components/TopNav';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Home from "./components/Home";
+import Nav from "./components/Nav";
+import TopNav from "./components/TopNav";
 
 class App extends Component {
   render() {
     return (
       <div>
         <Home />
-       {/* <div className="App">
+        {/* <div className="App">
         <header className="App-header"> */}
         <TopNav />
         <Nav />
-      {/* </header>
+        {/* </header>
       // </div>*/}
       </div>
     );
