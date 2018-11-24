f319c70759aaf04afa6f37ea1523fe95afc00af6
c1629177 <sulemana@cardiff.ac.uk>
2018-11-24 15:20:11 +0000

    Added state of current User and authenticated and export for app

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import setupFirebase from "./utils/firebaseSetup";
 import Dashboard from "./container/Dashboard/Dashboard";
 import {Redirect} from "react-router";
+import { app, base } from './base';
 
 function AuthenticatedRoute({component: Component, authenticated, ...rest}){
     console.log("redirecting");
@@ -22,16 +23,58 @@ function AuthenticatedRoute({component: Component, authenticated, ...rest}){
 class App extends Component {
   constructor(props) {
     super(props);
+    this.setCurrentUser = this.setCurrentUser.bind(this);
     setupFirebase();
+      this.state = {
+          authenticated: false,
+          currentUser: null
+      };
   }
+
+    setCurrentUser(user) {
+      console.log("setting current user");
+        if (user) {
+            this.setState({
+                currentUser: user,
+                authenticated: true
+            })
+        } else {
+            this.setState({
+                currentUser: null,
+                authenticated: false
+            })
+        }
+    }
+
+    componentWillMount() {
+        this.removeAuthListener = app.auth().onAuthStateChanged((user) => {
+            if (user) {
+                this.setState({
+                    authenticated: true,
+                    currentUser: user,
+                    loading: false,
+                })
+            } else {
+                this.setState({
+                    authenticated: false,
+                    currentUser: null,
+                    loading: false,
+                })
+            }
+        })
+    }
+
   render() {
     return (
       <div>
         <Router>
             <div className="sans-serif">
               <Route exact path="/" component={Home} />
-              <Route exact path="/login" component={Login} />
-                <AuthenticatedRoute exact path="/dashboard" component={Dashboard} />
+              {/*<Route exact path="/login" component={Login} />*/}
+                <Route exact path="/login" render={(props) => {
+                    return <Login setCurrentUser={this.setCurrentUser} {...props} />
+                }} />
+                <AuthenticatedRoute requireAuth={true} authenticated={this.state.authenticated} exact path="/dashboard" component={Dashboard} />
             </div>
           </Router>
       </div>
--- src/utils/firebaseSetup.js
+++ src/utils/firebaseSetup.js
@@ -2,12 +2,25 @@ import firebase from "firebase";
 import apikey from "../firebase.js";
 
 export default function setupFirebase() {
-  // Initializing app
-  let config = {
+    // Initializing app
+    const config = {
+        apiKey: apikey,
+        authDomain: "fir-test-6e084.firebaseapp.com",
+        databaseURL: "https://fir-test-6e084.firebaseio.com",
+        storageBucket: "fir-test-6e084.appspot.com"
+    };
+    firebase.initializeApp(config);
+
+}
+
+const config = {
     apiKey: apikey,
     authDomain: "fir-test-6e084.firebaseapp.com",
     databaseURL: "https://fir-test-6e084.firebaseio.com",
     storageBucket: "fir-test-6e084.appspot.com"
-  };
-  firebase.initializeApp(config);
-}
+};
+
+const app = firebase.initializeApp(config);
+
+export {app}
+
