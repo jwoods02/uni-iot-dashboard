b04cf9eed66b2aadc422cf174b997e29d69dab35
James Woods <woodsj13@cardiff.ac.uk>
2018-11-26 14:40:19 +0000

    Resolve promise hell to make graph work

--- src/components/Graph/Graph.js
+++ src/components/Graph/Graph.js
@@ -1,65 +1,61 @@
 import React from "react";
-import Plot from 'react-plotly.js';
-import graphData from "../../utils/graphQueries"
-
+import Plot from "react-plotly.js";
+import graphData from "../../utils/graphQueries";
 
 class Graph extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { data: [], layout: {}, frames: [], config: {} };
+  }
 
-    constructor(props) {
-        super(props);
-        this.state = { data: [], layout: {}, frames: [], config: {} };
-        this.setGraphData();
-    }
+  componentWillMount() {
+    this.setGraphData();
+  }
 
-    componentWillMount() {
+  setGraphData() {
+    // const theGraphData = graphData();
 
-    }
+    // console.log(theGraphData);
 
-    setGraphData(){
-        const graphData = graphData();
+    graphData().then(theGraphData => {
+      this.setState({
+        data: [
+          {
+            // todo this data will need to be set
+            x: theGraphData[0], // todo query for time
+            y: theGraphData[1], // todo query for sensor data
+            type: "line",
+            mode: "lines+points",
+            marker: { color: "red" }
+          }
+        ],
+        // todo set graph title with props
+        layout: {
+          width: 600,
+          height: 600,
+          title: "THIS WILL NEED TO BE SET WITH PROPS"
+        },
+        frame: {},
+        config: {}
+      });
+      this.forceUpdate();
+    });
+  }
 
-        this.setState({
-            data:
-                [{
-                // todo this data will need to be set
-                    x: graphData[0],  // todo query for time
-                    y: graphData[1], // todo query for sensor data
-                    type: 'line',
-                    mode: 'lines+points',
-                    marker: {color: 'red'}
-                }],
-            // todo set graph title with props
-            layout: {
-                width: 600, height: 600, title: "THIS WILL NEED TO BE SET WITH PROPS"
-            },
-            frame: {
-
-            },
-            config: {
-
-            }
-        })
-    }
-
-
-    render() {
-
-
-        return (
-            <div className={"chart"}>
-
-                <Plot
-                    data={this.state.data}
-                    layout={this.state.layout}
-                    frames={this.state.frames}
-                    config={this.state.config}
-                    onInitialized={(figure) => this.setState(figure)}
-                    onUpdate={(figure) => this.setState(figure)}
-                />
-
-            </div>
-        );
-    };
+  render() {
+    return (
+      <div className={"chart"}>
+        <Plot
+          data={this.state.data}
+          layout={this.state.layout}
+          frames={this.state.frames}
+          config={this.state.config}
+          onInitialized={figure => this.setState(figure)}
+          onUpdate={figure => this.setState(figure)}
+        />
+      </div>
+    );
+  }
 }
 
 export default Graph;
--- src/utils/graphQueries.js
+++ src/utils/graphQueries.js
@@ -1,41 +1,30 @@
 import firebase from "firebase";
 
-
-
-
-
 export default function graphData() {
+  let xAxis = [];
+  let yAxis = [];
 
-    let xAxis = [];
-    let yAxis = [];
+  const db = firebase.firestore();
+  db.settings({ timestampsInSnapshots: true });
 
-    const db = firebase.firestore();
-    db.settings({ timestampsInSnapshots: true });
-
-  db
-    .collection("sensor")
-    .get()
-    .then(snapshot => {
-      snapshot.docs.forEach(doc => {
+  return new Promise(function(resolve, reject) {
+    db.collection("sensor")
+      .get()
+      .then(snapshot => {
+        snapshot.docs.forEach(doc => {
           doc.data().readings.forEach(reading => {
-              xAxis.push(new Date(reading["date"]["seconds"]*1000));
-              yAxis.push(reading["value"])
-
+            xAxis.push(new Date(reading["date"]["seconds"] * 1000));
+            yAxis.push(reading["value"]);
           });
+        });
+        resolve([xAxis, yAxis]);
+      })
+      .catch(error => {
+        reject(error);
       });
+  });
 
-    });
-
-    return [xAxis, yAxis]
-
-
-    
-
-// todo will need a query to change sensors.sensor.lastUpdate
-    // todo check that you cant route to pages using the login
-    // https://www.youtube.com/watch?v=e8GA1UOj8mE&list=PL0vfts4VzfNiPCzuRPXFZS1Hnw_RvVEXR&index=9
-
-
-
+  // todo will need a query to change sensors.sensor.lastUpdate
+  // todo check that you cant route to pages using the login
+  // https://www.youtube.com/watch?v=e8GA1UOj8mE&list=PL0vfts4VzfNiPCzuRPXFZS1Hnw_RvVEXR&index=9
 }
-
