5bca2c9db45f77aae02f5ec71e5b8ddf8a6ffed5
James Woods <woodsj13@cardiff.ac.uk>
2018-11-27 18:23:59 +0000

    Fixed arduino server

--- src/server/arduino.js
+++ src/server/arduino.js
@@ -1,19 +1,32 @@
-const sp = require("serialport");
-const SerialPort = sp.SerialPort;
-const portName = process.argv[2];
+const SerialPort = require("serialport");
+const path = "/dev/cu.usbmodem14101";
+const admin = require("firebase-admin");
+const serviceAccount = require("../firebase.json");
 
-const myPort = new SerialPort(portName, {
-  baudRate: 9600,
-  parser: sp.parsers.readline("\r\n")
+const Readline = SerialPort.parsers.Readline;
+const port = new SerialPort(path);
+const parser = new Readline();
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  //the firebase db id
+  databaseURL: "https://fir-test-6e084.firebaseio.com"
 });
+const db = admin.firestore();
 
-myPort.on("open", onOpen);
-myPort.on("data", onData);
+const sensorRef = db.collection("sensor").doc("VFFRWeb6fwH6hfUu20NA");
 
-function onOpen() {
-  console.log("Opened Connection");
-}
+// Atomically add
+// let arrUnion = sensorRef.update({
+//   readings: admin.firestore.FieldValue.arrayUnion({ date, value })
+// });
 
-function onData(data) {
-  console.log("On data", data);
-}
+port.pipe(parser);
+parser.on("data", value => {
+  const date = new Date();
+  console.log(value);
+  console.log(date);
+  sensorRef.update({
+    readings: admin.firestore.FieldValue.arrayUnion({ date, value })
+  });
+});
