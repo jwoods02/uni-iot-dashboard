a352ac76a4815480ddea791b1aaf5dbc2e651c2e
Guy Rawsthorn <rawsthorng@cardiff.ac.uk>
2018-11-27 10:56:44 +0000

    Implementing collapsable Sidebar component - no test

--- src/container/Dashboard/Tables.js
+++ src/container/Dashboard/Tables.js
@@ -4,12 +4,36 @@ import TopNav from '../../components/Dashboard/TopNav';
 import Tables from '../../components/Dashboard/Tables';
 
 class Notifications extends Component {
+    constructor(props){
+        super(props);        
+            this.state = {
+              sidebarOpen: true
+            };
+
+        this.handleViewSidebar = this.handleViewSidebar.bind(this);
+    }
+    handleViewSidebar(){
+        this.setState({
+            sidebarOpen: !this.state.sidebarOpen});
+      }
     render() {
+        if (this.state.sidebarOpen === true) 
         return (
             <div>
-                <TopNav />
-                <div id="wrapper">
-                <Sidebar />
+            <TopNav onClick={this.handleViewSidebar}/>
+
+            <div id="wrapper">
+             <Sidebar isOpen={this.state.sidebarOpen} />
+             <Tables />
+             </div>
+         </div>
+        ) 
+        else 
+        return (
+            <div>
+               <TopNav onClick={this.handleViewSidebar}/>
+
+               <div id="wrapper">
                 <Tables />
                 </div>
             </div>
