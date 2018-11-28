dd8bb18791a197269f056c173f6e1c282e57d97b
Christopher Cardwell <CardwellCJ@cardiff.ac.uk>
2018-11-28 20:13:09 +0000

    Have fixed the container meaning that the sensors won't render properly in height

--- src/container/Dashboard/SensorView.js
+++ src/container/Dashboard/SensorView.js
@@ -1,19 +1,46 @@
 import React, { Component } from "react";
 import Sidebar from "../../components/Dashboard/Sidebar";
-import TopNav from "../../components/Dashboard/TopNav";
+import NavTop from "../../components/Dashboard/TopNav";
 import Sensors from "../../components/Dashboard/SensorView";
 
 class SensorView extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      sidebarOpen: true
+    };
+
+    this.handleViewSidebar = this.handleViewSidebar.bind(this);
+  }
+
+  handleViewSidebar() {
+    this.setState({
+      sidebarOpen: !this.state.sidebarOpen
+    });
+  }
+
   render() {
-    return (
-      <div>
-        <TopNav />
-        <div id="wrapper">
-          <Sidebar />
-          <Sensors />
+    if (this.state.sidebarOpen === true)
+      return (
+        <div>
+          <NavTop onClick={this.handleViewSidebar} />
+
+          <div id="wrapper">
+            <Sidebar isOpen={this.state.sidebarOpen} />
+            <Sensors />
+          </div>
         </div>
-      </div>
-    );
+      );
+    else
+      return (
+        <div>
+          <NavTop onClick={this.handleViewSidebar} />
+
+          <div id="wrapper">
+            <Sensors />
+          </div>
+        </div>
+      );
   }
 }
 
