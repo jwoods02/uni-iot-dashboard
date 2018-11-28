3bc7b308d299e41b37bf0a889e147d352a353313
Christopher Cardwell <CardwellCJ@cardiff.ac.uk>
2018-11-28 19:16:14 +0000

    Conditional images for the cards

--- src/components/Dashboard/SensorCard.js
+++ src/components/Dashboard/SensorCard.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { Link, Route, BrowserRouter } from "react-router-dom";
 import SensorTherm from "../../Assets/dht-11.png";
+import SensorLight from "../../Assets/tsl2561.png";
 // TODO: Get sensor image from type prop
 
 class SensorCard extends Component {
@@ -9,6 +10,39 @@ class SensorCard extends Component {
   }
 
   render() {
+    let card;
+    if (this.props.sensorType == "LIGHT") {
+      card = (
+        <div className="card-body">
+          <i className="fas fa-3x fa-lightbulb" />
+          <div className="logo-view">
+            <img
+              className="sensor-logo"
+              src={SensorLight}
+              alt="Sensor img not found"
+            />
+          </div>
+          <div className="card-text-bottom-l1">{this.props.sensorName}</div>
+          <div className="card-text-bottom-l2">{this.props.sensorType}</div>
+        </div>
+      );
+    } else if (this.props.sensorType == "TEMP") {
+      card = (
+        <div className="card-body">
+          <i className="fas fa-3x fa-temperature-high" />{" "}
+          <div className="logo-view">
+            <img
+              className="sensor-logo"
+              src={SensorTherm}
+              alt="Sensor img not found"
+            />
+          </div>
+          <div className="card-text-bottom-l1">{this.props.sensorName}</div>
+          <div className="card-text-bottom-l2">{this.props.sensorType}</div>
+        </div>
+      );
+    }
+
     return (
       <div className="col mb-4 ml-2 mt-3">
         <div className="card card-shadow text-white bg-danger o-hidden h-100">
@@ -17,22 +51,7 @@ class SensorCard extends Component {
               to="/dashboard/sensors"
               className=" text-white clearfix small z-1"
             >
-              <div className="card-body">
-                <i className="fas fa-3x fa-temperature-high" />
-                <div className="logo-view">
-                  <img
-                    className="sensor-logo"
-                    src={SensorTherm}
-                    alt="Sensor img not found"
-                  />
-                </div>
-                <div className="card-text-bottom-l1">
-                  {this.props.sensorName}
-                </div>
-                <div className="card-text-bottom-l2">
-                  {this.props.sensorType}
-                </div>
-              </div>
+              {card}
             </Link>
           </BrowserRouter>
         </div>
