407cfe29a54c590276e1152d8dbae28a74c34bf5
c1629177 <sulemana@cardiff.ac.uk>
2018-12-04 10:47:39 +0000

    sending emails with firebase cloud function and gmail

--- sendEmail.js
+++ sendEmail.js
@@ -1,24 +1,35 @@
-var nodemailer = require('nodemailer');
 
-var transporter = nodemailer.createTransport({
+const functions = require('firebase-functions');
+const nodemailer = require('nodemailer');
+const admin = require('firebase-admin');
+admin.initializeApp();
+
+
+// Configure the email transport using the default SMTP transport and a GMail account.
+// For other types of transports such as Sendgrid see https://nodemailer.com/transports/
+// TODO: Configure the `gmail.email` and `gmail.password` Google Cloud environment variables.
+// const gmailEmail = functions.config().gmail.email;
+// const gmailPassword = functions.config().gmail.password;
+
+const mailTransport = nodemailer.createTransport({
     service: 'gmail',
     auth: {
-        user: 'kabira.suleman@gmail.com',
-        pass: ''
-    }
+        user: "kabira.suleman@gmail.com",
+        pass: "mypass",
+    },
 });
 
-var mailOptions = {
-    from: 'kabira.suleman@gmail.com',
-    to: 'decobu@daabox.com',
-    subject: 'Sending Email using Node.js',
-    text: 'That was easy!'
-};
+// Sends an email confirmation when a user changes his mailing list subscription.
+exports.sendNotification = functions.auth.user().onCreate((user) => {
 
-transporter.sendMail(mailOptions, function(error, info){
-    if (error) {
-        console.log(error);
-    } else {
-        console.log('Email sent: ' + info.response);
-    }
+
+    const mailOptions = {
+        from: '"Spammy Corp." <noreply@firebase.com>',
+        to: "decobu@daabox.com",
+        text: "TEST"
+    };
+
+    return mailTransport.sendMail(mailOptions)
+        .then(() => console.log("It worked"))
+        .catch((error) => console.error('There was an error while sending the email:', error));
 });
