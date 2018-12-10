3f2ea998f750bf70a3090960c0b7d97a8eb38003
c1629177 <sulemana@cardiff.ac.uk>
2018-12-10 14:37:57 +0000

    email sent when sensor reading is updated

--- functions/index.js
+++ functions/index.js
@@ -4,24 +4,34 @@ const nodemailer = require('nodemailer');
 const mailTransport = nodemailer.createTransport({
     service: 'gmail',
     auth: {
-        user: "kabira.suleman@gmail.com",
-        pass: "fakepass",
+        user: "dwwiot9@gmail.com",
+        pass: "DataWorksWales9",
     },
 });
 
 // Sends an email confirmation when a user changes his mailing list subscription.
 exports.sendNotification = functions.firestore
-    .document('sensor/{sensorId}/readings')
-    .onUpdate((user) =>
+    .document('sensor/{readings}')
+    .onUpdate((change, context) => {
+        // Get an object representing the document
+        // e.g. {'name': 'Marie', 'age': 66}
+        const newValue = change.after.data();
+
+        // ...or the previous value before this update
+        const previousValue = change.before.data();
+
+        // access a particular field as you would any JS property
+            const name = newValue.value;
 
 
-    const mailOptions = {
-        from: '"ddw" <noreply@firebase.com>',
-        to: "kabira.suleman@gmail",
-        text: "Hello " + name + ", \r\m  You are receiving notification email because SENSOR_NAME is NOTIFICATION_TITLE. \r\m The notification message is as follows \r\m NOTIFICATION_MESSAGE."
-    };
+        const mailOptions = {
+            from: '"Data Works Wales" <noreply@firebase.com>',
+            subject: '*Sensor Notification*',
+            to: 'RawsthornG@cardiff.ac.uk',
+            text: "Hello PERSON_NAME   \r\n  You are receiving notification email because SENSOR_NAME is NOTIFICATION_TITLE. \r\n The notification message is as follows \r\n NOTIIFCATION_MESSAGE."
+        };
 
-    return mailTransport.sendMail(mailOptions)
-        .then(() => console.log("It worked"))
-        .catch((error) => console.error('There was an error while sending the email:', error));
+        return mailTransport.sendMail(mailOptions)
+            .then(() => console.log("It worked"))
+            .catch((error) => console.error('There was an error while sending the email:', error));
 });
