7bb668dd8e71c27d6964d42e8ff01ea341686555
c1629177 <sulemana@cardiff.ac.uk>
2018-12-11 14:50:48 +0000

    now getting collection ref for notincation

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,7 @@
 const functions = require('firebase-functions');
 const nodemailer = require('nodemailer');
+const admin = require('firebase-admin');
+admin.initializeApp();
 
 const mailTransport = nodemailer.createTransport({
     service: 'gmail',
@@ -14,16 +16,40 @@ exports.sendNotification = functions.firestore
     .document('sensor/{sensorId}')
     .onUpdate((change, context) => {
 
-        const sensorId = context.params.sensorId;
+        let sensorId = context.params.sensorId;
+        let notificationConditions = [];
+        let triggerConditions = [];
+        let docRef = admin.firestore().collection("sensor").doc(sensorId);
 
-
-
-        return console.log("sensor ID: " + sensorId.getData()  )
-
-
-
-
-});
+        console.log(docRef.collection("notification_conditions").doc(trigger_condition));
+        return new Promise(function(resolve, reject) {
+            docRef.collection("notification_conditions")
+                .get()
+                .then(conditions => {
+                    conditions.docs.forEach(doc => {
+                            notificationConditions.push(conditions);
+                    });
+                    resolve(notificationConditions);
+                    return new Promise(function(resolve, reject) {
+                        notificationConditions.collection("trigger_condition")
+                            .get()
+                            .then(conditions => {
+                                conditions.docs.forEach(doc => {
+                                    triggerConditions.push(conditions);
+                                });
+                                resolve(triggerConditions);
+                                return triggerConditions;
+                            })
+                            .catch(error => {
+                                reject(error);
+                            });
+                    })
+                })
+                .catch(error => {
+                    reject(error);
+                });
+        })
+    });
 
 //
 // const mailOptions = {
