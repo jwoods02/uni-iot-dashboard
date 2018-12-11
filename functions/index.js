ad7b2c6c785d9be3acf866fdfc614cdd9b4f9fab
c1629177 <sulemana@cardiff.ac.uk>
2018-12-11 11:30:34 +0000

    getting sensor ID dynamically

--- functions/index.js
+++ functions/index.js
@@ -11,27 +11,28 @@ const mailTransport = nodemailer.createTransport({
 
 // Sends an email confirmation when a user changes his mailing list subscription.
 exports.sendNotification = functions.firestore
-    .document('sensor/{readings}')
+    .document('sensor/{sensorId}')
     .onUpdate((change, context) => {
-        // Get an object representing the document
-        // e.g. {'name': 'Marie', 'age': 66}
-        const newValue = change.after.data();
 
-        // ...or the previous value before this update
-        const previousValue = change.before.data();
-
-        // access a particular field as you would any JS property
-            const name = newValue.value;
+        const sensorId = context.params.sensorId;
 
 
-        const mailOptions = {
-            from: '"Data Works Wales" <noreply@firebase.com>',
-            subject: '*Sensor Notification*',
-            to: 'RawsthornG@cardiff.ac.uk',
-            text: "Hello PERSON_NAME   \r\n  You are receiving notification email because SENSOR_NAME is NOTIFICATION_TITLE. \r\n The notification message is as follows \r\n NOTIIFCATION_MESSAGE."
-        };
 
-        return mailTransport.sendMail(mailOptions)
-            .then(() => console.log("It worked"))
-            .catch((error) => console.error('There was an error while sending the email:', error));
+        return console.log("sensor ID: " + sensorId.getData()  )
+
+
+
+
 });
+
+//
+// const mailOptions = {
+//     from: '"Data Works Wales" <noreply@firebase.com>',
+//     subject: '*Sensor Notification*',
+//     to: 'RawsthornG@cardiff.ac.uk',
+//     text: "Hello PERSON_NAME   \r\n  You are receiving notification email because SENSOR_NAME is NOTIFICATION_TITLE. \r\n The notification message is as follows \r\n NOTIIFCATION_MESSAGE."
+// };
+//
+// return mailTransport.sendMail(mailOptions)
+//     .then(() => console.log("It worked"))
+//     .catch((error) => console.error('There was an error while sending the email:', error));
