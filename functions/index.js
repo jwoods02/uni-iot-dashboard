c0e13f80384db7b2e73afe550a3151cb798ac608
James Woods <woodsj13@cardiff.ac.uk>
2018-12-12 15:39:37 +0000

    Updated cloud function. Working apart from email sending

--- functions/index.js
+++ functions/index.js
@@ -1,64 +1,77 @@
-const functions = require('firebase-functions');
-const nodemailer = require('nodemailer');
-const admin = require('firebase-admin');
+/* eslint-disable promise/no-nesting */
+const functions = require("firebase-functions");
+const nodemailer = require("nodemailer");
+const admin = require("firebase-admin");
 admin.initializeApp();
 
 const mailTransport = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-        user: "dwwiot9@gmail.com",
-        pass: "DataWorksWales9",
-    },
+  service: "gmail",
+  auth: {
+    user: "dwwiot9@gmail.com",
+    pass: "DataWorksWales9"
+  }
 });
 
 // Sends an email confirmation when a user changes his mailing list subscription.
 exports.sendNotification = functions.firestore
-    .document('sensor/{sensorId}')
-    .onUpdate((change, context) => {
+  .document("sensor/{sensorId}")
+  .onUpdate((change, context) => {
+    // Call with operators[key](3, 5)
+    let operators = {
+      ">": function(a, b) {
+        return a > b;
+      },
+      "<": function(a, b) {
+        return a < b;
+      },
+      "==": function(a, b) {
+        return a === b;
+      }
+    };
 
-        let sensorId = context.params.sensorId;
-        let notificationConditions = [];
-        let triggerConditions = [];
-        let docRef = admin.firestore().collection("sensor").doc(sensorId);
+    let notifCount = 0;
+    const sensorId = context.params.sensorId;
+    const docRef = admin
+      .firestore()
+      .collection("sensor")
+      .doc(sensorId);
 
-        console.log(docRef.collection("notification_conditions").doc(trigger_condition));
-        return new Promise(function(resolve, reject) {
-            docRef.collection("notification_conditions")
-                .get()
-                .then(conditions => {
-                    conditions.docs.forEach(doc => {
-                            notificationConditions.push(conditions);
-                    });
-                    resolve(notificationConditions);
-                    return new Promise(function(resolve, reject) {
-                        notificationConditions.collection("trigger_condition")
-                            .get()
-                            .then(conditions => {
-                                conditions.docs.forEach(doc => {
-                                    triggerConditions.push(conditions);
-                                });
-                                resolve(triggerConditions);
-                                return triggerConditions;
-                            })
-                            .catch(error => {
-                                reject(error);
-                            });
-                    })
-                })
-                .catch(error => {
-                    reject(error);
-                });
-        })
-    });
+    docRef
+      .get()
+      .then(doc => {
+        const theDoc = doc.data();
+        const notifications = theDoc.notification_conditions;
+        notifications.forEach(notif => {
+          notif
+            .get()
+            .then(docs => {
+              const data = theDoc.readings.slice(-1)[0].value;
+              const trigger = docs.data().trigger_condition;
+              const condition = trigger.split(" ");
 
-//
-// const mailOptions = {
-//     from: '"Data Works Wales" <noreply@firebase.com>',
-//     subject: '*Sensor Notification*',
-//     to: 'RawsthornG@cardiff.ac.uk',
-//     text: "Hello PERSON_NAME   \r\n  You are receiving notification email because SENSOR_NAME is NOTIFICATION_TITLE. \r\n The notification message is as follows \r\n NOTIIFCATION_MESSAGE."
-// };
-//
-// return mailTransport.sendMail(mailOptions)
-//     .then(() => console.log("It worked"))
-//     .catch((error) => console.error('There was an error while sending the email:', error));
+              console.log("The if statement", condition[0], data, condition[1]);
+
+              console.log(operators[condition[0]](data, condition[1]));
+
+              if (operators[condition[0]](data, condition[1])) {
+                notifCount += 1;
+                // SEND EMAIL HERE
+                console.log("le email");
+                return;
+              } else {
+                // DO NOT SEND EMAIL
+                console.log("no emailll");
+                return;
+              }
+            })
+            .catch(err => {
+              console.log(err);
+            });
+        });
+
+        return notifCount;
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  });
